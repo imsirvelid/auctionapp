0372d06bc776b012703966c6c82036ea8dd17ad4
velid <imsirvelid@gmail.com>
2023-03-15 00:10:08 +0100

    Create navigation card

---
+++ frontend/src/components/navigation-card/NavigationCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./navigationcard.css";
+
+function NavigationCard({ name, link, subLink }) {
+  return (
+    <div className="current-location">
+      <div style={{ display: "flex" }}>
+        <p style={{ "fontWeight": "500" }}>{name}</p>
+        <p style={{ "fontWeight": "100", "marginLeft": "auto" }}>
+          {link}/<span style={{ "fontWeight": "500", "marginLeft": "10px" }}>{subLink}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationCard;

352b197cea6007debc04eb6ba4aea0e1de5b58da
velid <imsirvelid@gmail.com>
2023-03-15 00:00:01 +0100

    Add routing for About us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Zdravo</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path="/" exact element={<h1>Test</h1>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 

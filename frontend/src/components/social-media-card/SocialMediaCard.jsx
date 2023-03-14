ebae7c57fcd4ab2e4f34d9bec32d1a9f0678d053
velid <imsirvelid@gmail.com>
2023-03-15 00:10:25 +0100

    Create social-media-card for social media icons

---
+++ frontend/src/components/social-media-card/SocialMediaCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./socialmediacard.css";
+
+function SocialMediaCard() {
+  return (
+    <div>
+      <a href="https://www.facebook.com"><i className="fa-brands fa-facebook first"></i></a>
+      <a href="https://www.instagram.com"><i className="fa-brands fa-instagram"></i></a>
+      <a href="https://www.twitter.com" ><i className="fa-brands fa-twitter"></i></a>
+      <a href="https://www.google.com"><i className="fa-brands fa-google-plus-g"></i></a>
+    </div>
+  );
+}
+
+export default SocialMediaCard;

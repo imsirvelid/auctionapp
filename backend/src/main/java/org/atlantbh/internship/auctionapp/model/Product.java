6dc5c907d8c314f2d3f374d740d6065ff611049d
velid <imsirvelid@gmail.com>
2023-03-15 14:57:04 +0100

    Create Product entity

--- backend/src/main/java/org/atlantbh/internship/auctionapp/model/Product.java
+++ backend/src/main/java/org/atlantbh/internship/auctionapp/model/Product.java
@@ -1,2 +1,42 @@
-package org.atlantbh.internship.auctionapp.model;public class Product {
+package org.atlantbh.internship.auctionapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "")
+    private User user;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "details")
+    private String details;
+
+    @Column(name = "created")
+    private LocalDateTime created;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+    
+    @Column(name = "status")
+    private STATUS status;
 }

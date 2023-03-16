0bc8c4e7785e7463945069807ab296c216ab930d
velid <imsirvelid@gmail.com>
2023-03-16 21:58:07 +0100

    Open link in new tab

--- frontend/src/components/social-media-card/SocialMediaCard.jsx
+++ frontend/src/components/social-media-card/SocialMediaCard.jsx
@@ -4,10 +4,10 @@ import "components/social-media-card/SocialMediaCard.css";
 function SocialMediaCard() {
   return (
     <div className="social-media-card">
-      <a href="https://www.facebook.com"><i className="fa-brands fa-facebook first i-social-media"></i></a>
-      <a href="https://www.instagram.com"><i className="fa-brands fa-instagram i-social-media"></i></a>
-      <a href="https://www.twitter.com" ><i className="fa-brands fa-twitter i-social-media"></i></a>
-      <a href="https://www.google.com"><i className="fa-brands fa-google-plus-g i-social-media"></i></a>
+      <a href="https://www.facebook.com" target="_blank" rel="noreferrer"><i className="fa-brands fa-facebook first i-social-media"></i></a>
+      <a href="https://www.instagram.com" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram i-social-media"></i></a>
+      <a href="https://www.twitter.com" target="_blank" rel="noreferrer"><i className="fa-brands fa-twitter i-social-media"></i></a>
+      <a href="https://www.google.com" target="_blank" rel="noreferrer"><i className="fa-brands fa-google-plus-g i-social-media"></i></a>
     </div>
   );
 }

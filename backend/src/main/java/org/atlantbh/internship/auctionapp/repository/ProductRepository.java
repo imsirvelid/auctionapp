7ed801a2746a1ff14d718a42e84ee64ad0dcde4d
velid <imsirvelid@gmail.com>
2023-03-16 20:20:37 +0100

    Create ProductRepository

--- backend/src/main/java/org/atlantbh/internship/auctionapp/repository/ProductRepository.java
+++ backend/src/main/java/org/atlantbh/internship/auctionapp/repository/ProductRepository.java
@@ -1,2 +1,10 @@
-package org.atlantbh.internship.auctionapp.repository;public class ProductRepository {
+package org.atlantbh.internship.auctionapp.repository;
+
+import org.atlantbh.internship.auctionapp.model.ProductEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
+
 }

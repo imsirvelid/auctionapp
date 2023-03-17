c83b80c6d8b0648cc34ffa5a2df3c9d187758250
velid <imsirvelid@gmail.com>
2023-03-17 23:32:14 +0100

    Load landing page if route is /

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "components/navbar/Navbar";
 import About from "pages/about/About";
 import TermsAndConditions from "pages/terms-and-conditions/TermsAndConditions";
 import PrivacyPolicy from "pages/privacy-policy/PrivacyPolicy";
+import Landing from "pages/landing/Landing";
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route path="/" exact element={<h1>Test</h1>} />
+          <Route path="/" exact element={<Landing />} />
           <Route path="/about" element={<About />} />
           <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />

0f09235641c2efa92fe5281460e0d8cef13f2a39
velid <imsirvelid@gmail.com>
2023-03-19 16:41:09 +0100

    Add tab to display new arrivals for product

--- frontend/src/components/tab-view/TabView.jsx
+++ frontend/src/components/tab-view/TabView.jsx
@@ -1,33 +1,10 @@
 import React, {useState, useEffect} from "react";
-
-import {getLatestProducts, getRandomProduct} from "api/Product";
 import "./TabView.css";
-import ProductGridCard from "components/product-grid-card/ProductGridCard";
-import InfiniteScroll from "react-infinite-scroll-component";
+
 
 function TabView(props) {
-  const [productList, setProductList] = useState([]);
   const tabs = props.tabs;
   const [currentTab, setCurrentTab] = useState(tabs[0].id);
-  const [page, setPage] = useState(1);
-  const [end, setEnd] = useState(false);
-  useEffect(() => {
-    const getLatest = async () => {
-      const res = await getLatestProducts(0);
-      setProductList(res);
-    };
-    getLatest();
-  }, []);
-
-  var fetchData = async () => {
-    if (end)
-      return;
-    const res = await getLatestProducts(page);
-    if (res.length == 0)
-      setEnd(true);
-    setProductList([...productList, ...res]);
-    setPage(page + 1);
-  }
 
   return (
     <>
@@ -48,30 +25,9 @@ function TabView(props) {
           <></>
         )}
       </div>
-      <InfiniteScroll className="infinite-scroll"
-        dataLength={productList.length}
-        next={fetchData}
-        hasMore={!end}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{textAlign: "center"}}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-      >
-        {productList
-          ? productList.map((product) => (
-              <div className="product-item" key={product.id}>
-                <ProductGridCard
-                  thumbnailUrl={product.thumbnailUrl}
-                  productTitle={product.name}
-                  startsFrom={product.startingPrice}
-                  key={product.id}
-                />
-              </div>
-            ))
-          : false}
-      </InfiniteScroll>
+      {tabs.map((tab) => (
+        tab.id === currentTab && tab.component
+      ))}
     </>
   );
 }

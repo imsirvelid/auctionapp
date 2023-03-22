30927079a0e38df3f0257419d26ee9048b1a1fc2
velid <imsirvelid@gmail.com>
2023-03-22 16:25:58 +0100

    Add route for getting one product by id

--- backend/src/main/java/org/atlantbh/internship/auctionapp/service/impl/ProductServiceImpl.java
+++ backend/src/main/java/org/atlantbh/internship/auctionapp/service/impl/ProductServiceImpl.java
@@ -1,15 +1,18 @@
 package org.atlantbh.internship.auctionapp.service.impl;
 
-import org.atlantbh.internship.auctionapp.controller.commons.PageParams;
-import org.atlantbh.internship.auctionapp.controller.commons.SortParams;
-import org.atlantbh.internship.auctionapp.entity.ProductEntity;
-import org.atlantbh.internship.auctionapp.model.Product;
+import org.atlantbh.internship.auctionapp.dto.Mapper.ProductMapper;
+import org.atlantbh.internship.auctionapp.dto.Product.ProductResponse;
+import org.atlantbh.internship.auctionapp.model.ProductEntity;
 import org.atlantbh.internship.auctionapp.repository.ProductRepository;
 import org.atlantbh.internship.auctionapp.service.api.ProductService;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,5 +34,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product getRandom() {
         return productRepository.findOneRandom().toDomainModel();
+    }
+
+    @Override
+    public ProductResponse getById(Long id) {
+        return productMapper.fromEntity(productRepository.findById(id).get());
     }
 }

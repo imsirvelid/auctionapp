24ae128c6e683f090c81af339d798bd157e5678a
velid <imsirvelid@gmail.com>
2023-03-23 21:35:10 +0100

    Remove uneccessary true

--- frontend/src/components/tab-view/TabView.jsx
+++ frontend/src/components/tab-view/TabView.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import "./TabView.css";
 
-
 function TabView(props) {
   const tabs = props.tabs;
   const [currentTab, setCurrentTab] = useState(0);
@@ -9,21 +8,17 @@ function TabView(props) {
   return (
     <>
       <div className="tab-view-header">
-        {true ? (
-          tabs.map((tab, index) => (
-            <p
-              className={`tab-view-option ${
-                index === currentTab ? "tvo-selected" : ""
-              }`}
-              key={tab.id}
-              onClick={() => setCurrentTab(index)}
-            >
-              {tab.title}
-            </p>
-          ))
-        ) : (
-          <></>
-        )}
+        {tabs.map((tab, index) => (
+          <p
+            className={`tab-view-option ${
+              index === currentTab ? "tvo-selected" : ""
+            }`}
+            key={tab.id}
+            onClick={() => setCurrentTab(index)}
+          >
+            {tab.title}
+          </p>
+        ))}
       </div>
       {tabs[currentTab].component}
     </>

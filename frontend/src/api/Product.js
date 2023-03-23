a776ce6f82a99332ca6bd588bafc79f3fa7ae1a3
velid <imsirvelid@gmail.com>
2023-03-23 22:13:34 +0100

    extract products for url

--- frontend/src/api/Product.js
+++ frontend/src/api/Product.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 import {BASE_URL} from "./Commons";
 
+const URL = BASE_URL + "/products";
+
 export const getLatestProducts = async (page, sortBy = "created") => {
-  const fetchData = await axios.get(BASE_URL + "/products", { params: { page: page, sort: sortBy }});
+  const fetchData = await axios.get(URL, { params: { page: page, sort: sortBy }});
   return fetchData.data;
 };
 
 export const getRandomProduct = async () => {
-  const data = await axios.get(BASE_URL + "/products/random");
+  const data = await axios.get(URL + "/random");
   return data.data;
 }

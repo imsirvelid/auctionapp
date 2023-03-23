8ebb5353dfa50779c9eacdc794425e33bdf9151e
velid <imsirvelid@gmail.com>
2023-03-23 21:46:37 +0100

    Use RequestMapping in controllers

--- backend/src/main/java/org/atlantbh/internship/auctionapp/controller/CategoryController.java
+++ backend/src/main/java/org/atlantbh/internship/auctionapp/controller/CategoryController.java
@@ -5,17 +5,19 @@ import org.atlantbh.internship.auctionapp.service.api.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping(value = "/categories/parent")
+    @GetMapping(value = "/parent")
     public ResponseEntity<List<CategoryResponse>> getParentCategories(){
         return ResponseEntity.ok(categoryService.getParentCategories());
     }
--- backend/src/main/java/org/atlantbh/internship/auctionapp/controller/ProductController.java
+++ backend/src/main/java/org/atlantbh/internship/auctionapp/controller/ProductController.java
@@ -6,23 +6,25 @@ import org.atlantbh.internship.auctionapp.service.api.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping(value = "/products", params = { "page", "sort"})
+    @GetMapping(value = "", params = { "page", "sort"})
     public ResponseEntity<List<ProductResponse>> getProducts(@RequestParam int page, @RequestParam String sort){
         return ResponseEntity.ok(productService.getAll(page, sort));
     }
 
-    @GetMapping(value = "/products/random")
+    @GetMapping(value = "/random")
     public ResponseEntity<ProductResponse> getRandomProduct() {
         return ResponseEntity.ok(productService.getRandom());
     }

d322f185b52b0b114937a0df244126fc4235002d
velid <imsirvelid@gmail.com>
2023-03-29 12:00:33 +0200

    add routes, fix date etc...

--- frontend/src/utils/DateHelper.js
+++ frontend/src/utils/DateHelper.js
@@ -1,9 +1,11 @@
 import moment from "moment";
 
 export const getDateDiffFromToday = (end) => {
-  var endDate = moment(end);
-  var today = moment();
-  var weeksBetween = endDate.diff(today, "week");
+  const endDate = moment(end);
+  const today = moment();
+  console.log("Danas je: ", today);
+  console.log("Menet interesuje: ", endDate);
+  const weeksBetween = endDate.diff(today, "week");
   today.add(weeksBetween, "weeks");
   return weeksBetween + " Weeks " + endDate.diff(today, "days") + " Days";
 };

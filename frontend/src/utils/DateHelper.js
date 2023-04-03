6a157b4d061acb530a09f5a562b979d5efa04d8d
velid <imsirvelid@gmail.com>
2023-04-03 09:50:36 +0200

    remove console logs

--- frontend/src/utils/DateHelper.js
+++ frontend/src/utils/DateHelper.js
@@ -3,8 +3,6 @@ import moment from "moment";
 export const getDateDiffFromToday = (end) => {
   const endDate = moment(end);
   const today = moment();
-  console.log("Danas je: ", today);
-  console.log("Menet interesuje: ", endDate);
   const weeksBetween = endDate.diff(today, "week");
   today.add(weeksBetween, "weeks");
   return weeksBetween + " Weeks " + endDate.diff(today, "days") + " Days";

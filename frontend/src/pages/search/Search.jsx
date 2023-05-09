8a4cd8d913f2e6636babb4419b4717a84d92b728
velid <imsirvelid@gmail.com>
2023-05-09 23:42:34 +0200

    Create custom-select component and sorting filter on search page

--- frontend/src/pages/search/Search.jsx
+++ frontend/src/pages/search/Search.jsx
@@ -4,7 +4,9 @@ import Button from "components/button/Button";
 import ProductGridCard from "components/product-grid-card/ProductGridCard";
 import React, {useState, useEffect} from "react";
 import {Link, useLocation} from "react-router-dom";
+import {Formik, Form, Field, ErrorMessage} from "formik";
 import "./Search.css";
+import CustomSelect from "components/custom-select/CustomSelect";
 
 function Search() {
   const [categories, setCategories] = useState([]);
@@ -15,6 +17,42 @@ function Search() {
   const [categoryId, setCategory] = useState();
   const [didYouMean, setDidYouMean] = useState();
   const location = useLocation();
+  const [sorting, setSorting] = useState(0);
+
+  const sortingItems = [
+    {value: 0, label: "Default Sorting"},
+    {value: 1, label: "Added: New to Old"},
+    {value: 2, label: "Time left"},
+    {value: 3, label: "Price: Low to High"},
+    {value: 4, label: "Price: High to Low"},
+  ];
+
+  const onSortingChange = (value) => {
+    setSorting(value.value);
+    const sortingParams = [
+      {field: "created", order: "desc"},
+      {field: "created", order: "desc"},
+      {field: "endDate", order: "asc"},
+      {field: "startingPrice", order: "asc"},
+      {field: "startingPrice", order: "desc"},
+    ];
+    setNextPage(1);
+    const getProducts = async () => {
+      const res = await searchProducts(
+        0,
+        sortingParams[value.value].field,
+        sortingParams[value.value].order,
+        9,
+        name,
+        categoryId
+      );
+      setProducts(res.page.content);
+      setDidYouMean(res.didYouMeanSuggestion);
+      setHasMore(!res.page.last);
+    };
+    getProducts();
+  };
+
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     setNextPage(1);
@@ -38,8 +76,7 @@ function Search() {
       setDidYouMean(res.didYouMeanSuggestion);
       setHasMore(!res.page.last);
     };
-    if (categories.length === 0)
-      getCategories();
+    if (categories.length === 0) getCategories();
     getProducts();
   }, [location, categories.length]);
 
@@ -60,18 +97,19 @@ function Search() {
 
   return (
     <>
-    { 
-      didYouMean && (
-      <div className="did-you-mean-breadcrumb">
-        <p>
-          Did you mean?{" "}
-          <Link className="did-you-mean-link" to={`/search?name=${didYouMean}`}>
-            {didYouMean}
-          </Link>
-        </p>
-      </div>
-      )
-    }
+      {didYouMean && (
+        <div className="did-you-mean-breadcrumb">
+          <p>
+            Did you mean?{" "}
+            <Link
+              className="did-you-mean-link"
+              to={`/search?name=${didYouMean}`}
+            >
+              {didYouMean}
+            </Link>
+          </p>
+        </div>
+      )}
       <div className="container">
         <div className="main-content-search">
           <div className="categories-container">
@@ -97,6 +135,12 @@ function Search() {
             <div className="empty-div"></div>
           </div>
           <div className="right-search-content">
+            <div className="search-sorting-select">
+              <CustomSelect
+                items={sortingItems}
+                onChange={onSortingChange}
+              ></CustomSelect>
+            </div>
             <div className="search-products-grid">
               {products.length > 0 ? (
                 products.map((product) => (
@@ -109,7 +153,9 @@ function Search() {
                     />
                   </div>
                 ))
-              ) : ( <p>No products found</p>)}
+              ) : (
+                <p>No products found</p>
+              )}
             </div>
             {hasMore && (
               <Button type="explore-more-btn purple" onClick={exploreMore}>

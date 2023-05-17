c41d815ecaa8811cb726b8b6c7c54dfec5de020f
velid <imsirvelid@gmail.com>
2023-05-17 10:18:03 +0200

    resolve comments

--- frontend/src/api/User.js
+++ frontend/src/api/User.js
@@ -8,10 +8,6 @@ export const loginUser = async (request) => {
   return fetchData.data;
 };
 
-export const getCurrentUser = async () => {
-  return axios.get(URL + "/getuser");
-};
-
 export const registerUser = async (request) => {
   const fetchData = await axios.post(URL + "/register", request);
   return fetchData.data;
